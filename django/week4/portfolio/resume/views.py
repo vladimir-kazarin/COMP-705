33697511144274f36e89b5523ed4b3e03a08af7c
vladimir-kazarin <vladimir_kazarin@yahoo.com>
2018-02-17 21:48:57 -0500

    Django Week4 Changing views and templates to use Resume class

--- django/week4/portfolio/resume/views.py
+++ django/week4/portfolio/resume/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
-from .models import Education, Experience
+from .models import Education, Experience, Resume
 
 # Create your views here.
 def home(request):
     '''
     Renders the Resume app home page
     '''
-    education_qs = Education.objects.all()
-    experience_qs = Experience.objects.order_by('-start_date')
-    context = {'education_list': education_qs,
-     'experience_list': experience_qs}
+    resume = Resume.objects.first()
+    context = {'resume': resume}
     return render(request, 'resume/home.html', context) 
